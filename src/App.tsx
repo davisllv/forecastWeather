4694cf23ca7c5442a746d883c010250dc12e28c8
davisllv <davisllvsnt57@gmail.com>
2024-01-30 20:36:26 -0300

    created the initial page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import { IoSearchOutline } from "react-icons/io5";
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-image: linear-gradient(#ff8970, #ffa08b);
+`;
+
+const ContentContainer = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+  align-items: center;
+  border: 1px solid black;
+  min-width: 50%;
+  min-height: 80%;
+`;
+
+const MainTitle = styled.h1`
+  color: #fff;
+  margin: 5rem 0;
+  font-size: 3rem;
+`;
+
+const InputContainer = styled.div`
+  width: 70%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  padding-bottom: 4rem;
+  border-bottom: 1px solid white;
+`;
+
+const InputComponent = styled.input`
+  padding: 1rem;
+  width: 60%;
+  border-radius: 4px;
+  border: none;
+  outline: none;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainContainer>
+      <ContentContainer>
+        <MainTitle>Previsão do Tempo.</MainTitle>
+        <InputContainer>
+          <InputComponent placeholder="Insira o nome da cidade aqui" />
+          <IoSearchOutline
+            size={24}
+            style={{
+              position: "relative",
+              left: "-32px",
+              cursor: "pointer",
+            }}
+            onClick={() => console.log("Oi davi")}
+          />
+        </InputContainer>
+      </ContentContainer>
+    </MainContainer>
   );
 }
 
