8b13dd5ab73991794b462f422cf037685aaa8246
davisllv <davisllvsnt57@gmail.com>
2024-01-30 21:29:20 -0300

    make some another improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { IoSearchOutline } from "react-icons/io5";
 
-const MainContainer = styled.div`
+const flexContainer = css`
   display: flex;
   justify-content: center;
   align-items: center;
+`;
+
+const MainContainer = styled.div`
+  ${flexContainer}
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
   height: 100vh;
   background-image: linear-gradient(#ff8970, #ffa08b);
 `;
-
+console.log();
 const ContentContainer = styled.div`
-  display: inline-flex;
-  flex-direction: column;
-  align-items: center;
+  ${flexContainer
+    .join("")
+    .replace("justify-content: center;", "flex-direction: column; ")
+    .split(" ")}
   border: 1px solid black;
   min-width: 50%;
   min-height: 80%;
@@ -21,26 +27,58 @@ const ContentContainer = styled.div`
 
 const MainTitle = styled.h1`
   color: #fff;
-  margin: 5rem 0;
-  font-size: 3rem;
+  margin: 4rem 0;
+  font-size: 2.5rem;
 `;
 
 const InputContainer = styled.div`
   width: 70%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
+  ${flexContainer}
   padding-bottom: 4rem;
   border-bottom: 1px solid white;
 `;
 
 const InputComponent = styled.input`
   padding: 1rem;
-  width: 60%;
+  width: 80%;
   border-radius: 4px;
   border: none;
   outline: none;
+`;
+
+const CapitalContainer = styled.div`
+  margin-top: 2rem;
+  width: 65%;
+
+  > h2 {
+    font-size: 1rem;
+    color: #fff;
+    margin-bottom: 2rem;
+  }
+`;
+
+const CapitalTableContainer = styled.div`
+  display: flex;
+  gap: 1.5rem;
+  flex-direction: row;
+`;
+
+const TableContainer = styled.table`
+  font-weight: bold;
+
+  > tr > th {
+    padding: 0.75rem 0.5rem 0.5rem 0;
+    font-size: 12px;
+  }
+
+  > tr > td {
+    padding: 0.5rem 0.5rem 0.5rem 0;
+    font-size: 14px;
+  }
+
+  > tr > td:last-child {
+    padding-left: 12px;
+  }
 `;
 
 function App() {
@@ -60,6 +98,77 @@ function App() {
             onClick={() => console.log("Oi davi")}
           />
         </InputContainer>
+
+        <CapitalContainer>
+          <h2>Capitais</h2>
+
+          <CapitalTableContainer>
+            <TableContainer>
+              <tr>
+                <th>Min</th>
+                <th>Max</th>
+                <th></th>
+              </tr>
+              <tr>
+                <td>10º</td>
+                <td>15º</td>
+                <td>Porto Alegre</td>
+              </tr>
+              <tr>
+                <td>10º</td>
+                <td>15º</td>
+                <td>Porto Alegre</td>
+              </tr>
+              <tr>
+                <td>10º</td>
+                <td>15º</td>
+                <td>Porto Alegre</td>
+              </tr>
+              <tr>
+                <td>10º</td>
+                <td>15º</td>
+                <td>Porto Alegre</td>
+              </tr>
+              <tr>
+                <td>10º</td>
+                <td>15º</td>
+                <td>Porto Alegre</td>
+              </tr>
+            </TableContainer>
+            <TableContainer>
+              <tr>
+                <th>Min</th>
+                <th>Max</th>
+                <th></th>
+              </tr>
+              <tr>
+                <td>10º</td>
+                <td>15º</td>
+                <td>Porto Alegre</td>
+              </tr>
+              <tr>
+                <td>10º</td>
+                <td>15º</td>
+                <td>Porto Alegre</td>
+              </tr>
+              <tr>
+                <td>10º</td>
+                <td>15º</td>
+                <td>Porto Alegre</td>
+              </tr>
+              <tr>
+                <td>10º</td>
+                <td>15º</td>
+                <td>Porto Alegre</td>
+              </tr>
+              <tr>
+                <td>10º</td>
+                <td>15º</td>
+                <td>Porto Alegre</td>
+              </tr>
+            </TableContainer>
+          </CapitalTableContainer>
+        </CapitalContainer>
       </ContentContainer>
     </MainContainer>
   );
